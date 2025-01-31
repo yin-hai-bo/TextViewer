7eab847ee0f5009c85754116a8a8c89cd0577c8b
yinhaibo <nosound@163.com>
2025-01-31 23:17:13 +0800

    feat: Translate UI message to Chinese

--- TextViewer_zh_CN.ts
+++ TextViewer_zh_CN.ts
@@ -5,225 +5,190 @@
     <name>AboutBox</name>
     <message>
         <location filename="aboutbox.ui" line="17"/>
-        <location filename="aboutbox.ui" line="17"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
     <message>
-        <location filename="aboutbox.ui" line="54"/>
         <location filename="aboutbox.ui" line="54"/>
         <source>A simple text document viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>一个简单的纯文本文档的查看程序</translation>
     </message>
     <message>
         <location filename="aboutbox.ui" line="64"/>
-        <location filename="aboutbox.ui" line="64"/>
         <source>&lt;a href=&quot;https://github.com/yin-hai-bo/TextViewer&quot;&gt;https://github.com/yin-hai-bo/TextViewer&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
     <name>LineHeightDialog</name>
     <message>
         <location filename="lineheightdialog.ui" line="14"/>
-        <location filename="lineheightdialog.ui" line="14"/>
-        <source>Lien Height Settings</source>
-        <translation type="unfinished"></translation>
+        <source>Line Height Settings</source>
+        <translation>行间距设置</translation>
     </message>
     <message>
         <location filename="lineheightdialog.ui" line="64"/>
-        <location filename="lineheightdialog.ui" line="64"/>
         <source>Line Height  (%):</source>
-        <translation type="unfinished"></translation>
+        <translation>行间距 (%):</translation>
     </message>
 </context>
 <context>
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="mainwindow.ui" line="14"/>
         <source>Text Viewer</source>
-        <translation type="unfinished"></translation>
+        <translation>文本查看器</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="50"/>
         <location filename="mainwindow.ui" line="50"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="59"/>
         <location filename="mainwindow.ui" line="59"/>
         <source>&amp;View</source>
-        <translation type="unfinished"></translation>
+        <translation>查看(&amp;V)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="71"/>
         <location filename="mainwindow.ui" line="71"/>
         <source>&amp;Help</source>
-        <translation type="unfinished"></translation>
+        <translation>帮助(&amp;H)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="82"/>
         <location filename="mainwindow.ui" line="82"/>
         <source>toolBar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="102"/>
-        <location filename="mainwindow.ui" line="102"/>
         <source>&amp;Open...</source>
-        <translation type="unfinished"></translation>
+        <translation>打开(&amp;O)...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="105"/>
         <location filename="mainwindow.ui" line="105"/>
         <source>Open a text file to view.</source>
-        <translation type="unfinished"></translation>
+        <translation>打开一个文本文档进行查看</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="108"/>
         <location filename="mainwindow.ui" line="108"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="113"/>
         <location filename="mainwindow.ui" line="113"/>
         <source>E&amp;xit</source>
-        <translation type="unfinished"></translation>
+        <translation>退出(&amp;X)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="116"/>
         <location filename="mainwindow.ui" line="116"/>
         <source>Exit this application.</source>
-        <translation type="unfinished"></translation>
+        <translation>退出本程序</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="124"/>
         <location filename="mainwindow.ui" line="124"/>
         <source>C&amp;lose</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭(&amp;L)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="127"/>
         <location filename="mainwindow.ui" line="127"/>
         <source>Close current document.</source>
-        <translation type="unfinished"></translation>
+        <translation>关闭当前文档</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="130"/>
         <location filename="mainwindow.ui" line="130"/>
         <source>Ctrl+F4</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="141"/>
         <location filename="mainwindow.ui" line="141"/>
         <source>Zoom &amp;In</source>
-        <translation type="unfinished"></translation>
+        <translation>放大(&amp;I)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="144"/>
         <location filename="mainwindow.ui" line="144"/>
         <source>Ctrl+=</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="152"/>
         <location filename="mainwindow.ui" line="152"/>
         <source>Zoom &amp;Out</source>
-        <translation type="unfinished"></translation>
+        <translation>缩小(&amp;O)</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="155"/>
-        <location filename="mainwindow.ui" line="155"/>
         <source>Ctrl+-</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="160"/>
         <location filename="mainwindow.ui" line="160"/>
         <source>&amp;Font...</source>
-        <translation type="unfinished"></translation>
+        <translation>字体(&amp;F)...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="168"/>
         <location filename="mainwindow.ui" line="168"/>
         <source>Restore default zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>恢复到默认缩放大小</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="171"/>
         <location filename="mainwindow.ui" line="171"/>
         <source>&amp;Restore the default zoom (100%)</source>
-        <translation type="unfinished"></translation>
+        <translation>恢复到默认缩放 100% (&amp;R)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="174"/>
         <location filename="mainwindow.ui" line="174"/>
         <source>Ctrl+0</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="182"/>
         <location filename="mainwindow.ui" line="182"/>
         <source>&amp;About</source>
-        <translation type="unfinished"></translation>
+        <translation>关于(&amp;A)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="187"/>
         <location filename="mainwindow.ui" line="187"/>
         <source>Line &amp;Height...</source>
-        <translation type="unfinished"></translation>
+        <translation>行间距(&amp;H)...</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="190"/>
         <location filename="mainwindow.ui" line="190"/>
         <source>Setting the line height</source>
-        <translation type="unfinished"></translation>
+        <translation>设置行间距</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="73"/>
         <location filename="mainwindow.cpp" line="73"/>
         <source>Open a text file to view</source>
-        <translation type="unfinished"></translation>
+        <translation>打开一个文本文档进行查看</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="75"/>
         <location filename="mainwindow.cpp" line="75"/>
         <source>Text files (*.txt);;All files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>文本文件 (*.txt);;所有文件 (*)</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="89"/>
         <location filename="mainwindow.cpp" line="89"/>
         <source>Open file failed.</source>
-        <translation type="unfinished"></translation>
+        <translation>打开文档失败。</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="95"/>
         <location filename="mainwindow.cpp" line="95"/>
         <source>It&apos;s an empty file.</source>
-        <translation type="unfinished"></translation>
+        <translation>空文档。</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="100"/>
         <location filename="mainwindow.cpp" line="100"/>
         <source>Too large file.</source>
-        <translation type="unfinished"></translation>
+        <translation>过大的文件。</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="112"/>
         <location filename="mainwindow.cpp" line="112"/>
         <source>Text size: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>文档大小：%1</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="131"/>
-        <location filename="mainwindow.cpp" line="131"/>
         <source>(No document)</source>
-        <translation type="unfinished"></translation>
+        <translation>（尚未打开文档）</translation>
     </message>
 </context>
 </TS>
